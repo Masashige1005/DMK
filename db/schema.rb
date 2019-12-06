d0688f73242ecf73ac1952fdfd0317e5eff602c7
Masashige1005 <ineimasashige@gmail.com>
2019-12-06 15:11:39 +0900

    bootstrap install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_115801) do
+ActiveRecord::Schema.define(version: 2019_12_05_093259) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.integer "song_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name", null: false
